641fce26d583551b8037a17d67c0f7d90fa2546d
Denis <denis.harbiyanto@ansvia.com>
2021-08-20 23:54:02 +0700

    [ADD] development script

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/views/**/*.{html,ejs}", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
